be53cec6004027e42eae7aa139ec004dce117e26
Pravin-Hexaware <2000160744@hexaware.com>
2025-08-22 14:08:23 +0530

    Delete Backend/main directory

--- Backend/main/java/com/pms/backend/repository/AssetClassRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pms.backend.repository;
-
-import com.pms.backend.entity.AssetClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssetClassRepository extends JpaRepository<AssetClass, Long> {
-
-}
--- Backend/main/java/com/pms/backend/repository/PortfolioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pms.backend.repository;
-
-import com.pms.backend.entity.Portfolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-}
